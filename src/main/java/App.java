be31b3c62efeb79c6ff15c49fa719bc158663568
Antony Voloshin <49843612+tohamdhope@users.noreply.github.com>
2022-07-19 22:31:28 +0300

    hello again coc..er

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBeanOne =
+                (Cat) applicationContext.getBean("cat");
+        Cat catBeanTwo =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == secondBean);
+        System.out.println(catBeanOne == catBeanTwo);
     }
 }
